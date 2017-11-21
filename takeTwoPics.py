49aa0b1d9352e1f925e29049a19cdc56e1f8ab60
Jonah Weissman <jonahrweissman@gmail.com>
2017-11-21 12:15:31 -0500

    Change to take pic every 5 seconds
    Add command line args and print statement
    Rename basic_control.py

--- takeTwoPics.py
+++ takeTwoPics.py
@@ -1,26 +1,31 @@
 import cv2
+import sys
+import time
+from datetime import datetime
 
 cam1 = cv2.VideoCapture(0)
 cam2 = cv2.VideoCapture(1)
 
-# Get enumerations witht he expression cv2.cv.YOUR_ENUM
-#cam1.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'))
-#cam2.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'))
+# Get enumerations with the expression cv2.cv.YOUR_ENUM
 cam1.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 160)
 cam1.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 120)
 cam2.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 160)
 cam2.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 120)
-#cam1.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 5)
-#cam2.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 5)
 
-def takePic(camera, file):
-   _, img = camera.read()
-   return cv2.imwrite(file, img)
+numCycles = int(sys.argv[1] if sys.argv[1] else 100)
+pauseTime = int(sys.argv[2] if sys.argv[2] else 5)
 
-#path = '/home/odroid/ImageTesting/'
-print(takePic(cam1, 'test1.png'))
-print(takePic(cam2, 'test2.png'))
+for i in range(1, numCycles):
+  _, img1 = cam1.read()
+  _, img2 = cam2.read()
+  time = str(datetime.now().time())
+  path = '/home/odroid/images/' + time
+  cv2.imwrite(path+'-1.png', img1)
+  cv2.imwrite(path+'-2.png', img2)
+  print("Saved file pair %d at %s" % (i, time))
+  time.sleep(pauseTime)
 
+# If we don't release the cameras, bad things could happen
 cam1.release()
 cam2.release()
 del(cam1)
