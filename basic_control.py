e83bac037077e7df8e99948a545a93f0421de4a6
Jonah Weissman <jonahrweissman@gmail.com>
2017-12-06 11:41:14 -0500

    Add print statement

--- basic_control.py
+++ basic_control.py
@@ -3,7 +3,9 @@ from time import sleep
 drone=dronekit.connect("/dev/ttyUSB0")
 drone.groundspeed=1 #m/s
 condition_yaw(0)
+print("sending ned")
 dronekit.send_ned_velocity(.2,0,0) #Coordinates are north, east, down (so south, west, up are negative)
+print("finished sending ned")
 #sleep(10)
 #dronekit.send_ned_velocity(0,0,0)
 #vehicle.mode = VehicleMode("LAND")
